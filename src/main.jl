db019171bc4b1badc65aa59a69ace5c2d6f7ea4d
Martin Cornejo <martin.cornejo@tum.de>
2024-01-25 16:38:48 +0100

    First draft main script

--- src/main.jl
+++ src/main.jl
@@ -1,76 +1,35 @@
 using CSV
 using DataFrames
 using DataInterpolations
+using StatsBase
 
 using ModelingToolkit
 using DifferentialEquations
-
-using Stheno
 
 using Optimization
 using OptimizationOptimJL
 import ComponentArrays: ComponentArray
 
+using Stheno
+using ParameterHandling
+using Zygote
+
 # using GLMakie
 using CairoMakie
-CairoMakie.activate!()
+# CairoMakie.activate!(type="svg")
 
 include("checkup.jl")
 include("ecm.jl")
+include("gp.jl")
 
+# ------
+files = readdir("data/check-ups/", join=true)
 
-#
-function plot_ocvs(files)
-    s = 0:0.001:1.0
-    ylabel = "Voltage in V"
-    xlabel = "SOC in p.u."
-    colormap = :dense
-    colorrange = (0.6, 1.0)
+fig = plot_checkup_profile(files[end]) # fresh cell
+# save("figs/measurement-plan-2.pdf", fig, pt_per_unit=1)
+# fig |> save("figs/measurement-plan-2.pdf")
 
-    fig1 = Figure(resolution=(800, 400))
-    ax1 = Axis(fig1[1, 1]; xlabel, ylabel, title="Charge pOCV")
-    ax2 = Axis(fig1[1, 2]; xlabel, title="Discharge pOCV")
-    linkyaxes!(ax1, ax2)
-    hideydecorations!(ax2, ticks=false, grid=false)
-
-    fig2 = Figure(resolution=(900, 400))
-    ax3 = Axis(fig2[1, 1]; xlabel, ylabel, title="Mean pOCV")
-    # ax4 = Axis(fig2[1, 2]; xlabel, title="OCV Hystheresis")
-    ax4 = Axis(fig2[1, 2]; xlabel, title="Deviation")
-
-    focv = fresh_focv()
-
-    for file in files
-        df = read_basytec(file)
-
-        # c-cov
-        ocv_c, cap_c = calc_cocv(df)
-        c = s .* cap_c
-        lines!(ax1, s, ocv_c(c); color=cap_c / 4.9, colorrange, colormap)
-
-        # d-ocv
-        ocv_d, cap_d = calc_docv(df)
-        c = s .* cap_d
-        lines!(ax2, s, ocv_d(c); color=cap_d / 4.9, colorrange, colormap)
-
-        # ocv
-        pocv, cap = calc_pocv(df)
-        c = s .* cap
-        lines!(ax3, s, pocv(c); color=cap / 4.9, colorrange, colormap)
-
-        # hyst
-        # lines!(ax4, s, pocv(c) - ocv_c(c); color=cap / 4.9, colorrange, colormap)
-
-        # degradation
-        if !occursin("LGL13818", file)
-            lines!(ax4, s, pocv(c) - focv(s); color=cap / 4.9, colorrange, colormap)
-        end
-    end
-
-    Colorbar(fig1[:, 3]; colorrange, colormap, label="SOH")
-    Colorbar(fig2[:, 3]; colorrange, colormap, label="SOH")
-    return fig1, fig2
-end
+fig = plot_ocvs(files)
 
 
 # ------
@@ -95,7 +54,7 @@ end
 function plot_ecm_series(res)
     tspan = (0.0, 3 * 24 * 3600.0)
 
-    fig = Figure(resolution=(1200, 600))
+    fig = Figure(size=(1200, 600))
     ax = [Axis(fig[i, 1]) for i in 1:3]
     ax[1].title = "Measured"
     ax[2].title = "Simulated"
@@ -126,7 +85,7 @@ function plot_ecm_series(res)
         df_cell = read_basytec(file)
         data = load_profile(df_cell)
 
-        cap = prob = res[Symbol(id)][:cu]
+        cap = res[Symbol(id)][:cu]
         soh = cap / 4.9
 
         prob = res[Symbol(id)][:ode]
@@ -150,12 +109,11 @@ function plot_ecm_series(res)
     fig
 end
 
-files = readdir("data/check-ups/", join=true)
-
 # ocvs
-fig1, fig2 = plot_ocvs(files)
-fig1 |> save("figs/ocv1.svg")
-fig2 |> save("figs/ocv2.svg")
+fig1, fig2, fig3 = plot_ocvs(files)
+fig1 |> save("figs/ocv1.svg") # charge/discharge ocv
+fig2 |> save("figs/ocv2.svg") # mean ocv + hystheresis
+fig3 |> save("figs/ocv3.svg") # mean ocv + degradation
 
 # ECM
 res = fit_ecm_series(files)
@@ -174,15 +132,6 @@ fig |> save("figs/ecm.svg")
 
 
 ### GP
-θ = (;
-    kernel=(;
-        ocv=(; σ=positive(0.1), l=positive(0.1)),
-        # hys=(; σ=positive(0.1), l=positive(0.1)),
-        r=(; σ=positive(0.1), l=positive(0.1))
-        # r=positive(0.1)
-    ),
-    noise=positive(1e-5)
-)
 "SOC dependent R"
 function model(θ)
     i = x -> x[2]
@@ -194,98 +143,11 @@ function model(θ)
     end
 end
 
+function fit_gp_series(files)
+    tt = 0:60:(24*3600.0)
 
-function fit_gp_hyperparams(model, θ, df)
-
-    ŝ = df.s / 4.9
-    î = df.i ./ 4.9
-    v̂ = df.v .- 3.7
-    x = GPPPInput(:v, RowVecs([ŝ î]))
-
-    loss = create_loss_function(model, x, v̂)
-    θ0, unflatten = ParameterHandling.value_flatten(θ)
-    loss_packed = loss ∘ unflatten
-
-    adtype = AutoZygote()
-    f = OptimizationFunction((u, p) -> loss_packed(u), adtype)
-    prob = OptimizationProblem(f, θ0)
-
-    sol = solve(prob, LBFGS(); show_trace=true)
-    θ_opt = unflatten(sol.u)
-
-    return θ_opt
-end
-
-
-# θ = fit_gp_hyperparams(model, θ, df_train)
-θ = (
-    kernel=(
-        ocv=(σ=0.11650161909960115, l=0.07224889831676123),
-        r=(σ=0.11086743740675992, l=0.0774432873218997)
-    ),
-    noise=2.8383204772342602e-5
-)
-
-fp = model(θ.kernel)
-fx = fp(x, θ.noise)
-gp = posterior(fx, v̂)
-
-
-function plot_gp_ocv(gp, focv, cap, df, ax1, ax2)
-    soh = cap / 4.9
-    s = 0:0.01:1
-    s2 = (s .- 0.385) .* soh
-    i = zeros(size(s))
-    data = RowVecs([s2 i])
-    x = GPPPInput(:ocv, data)
-    ocv_gp = gp(x)
-    ocvμ = mean(ocv_gp) .+ 3.7
-    ocvσ = sqrt.(var(ocv_gp))
-
-    ocv_meas = focv.(s * cap)
-
-    # fig = Figure()
-
-    e = [extrema(df.s)...]
-    vl = (e ./ 4.9) .+ 0.385
-
-    ax1 = Axis(fig[1, 1])
-    ax1.ylabel = "OCV (V)"
-    xlims!(ax1, (0, 1))
-    ylims!(ax1, (3.2, 4.2))
-    lines!(ax1, s, ocvμ)
-    lines!(ax1, s, ocv_meas)
-    band!(ax1, s, ocvμ - ocvσ, ocvμ + ocvσ)
-    vlines!(ax1, vl, color=(:black, 0.5), linestyle=:dash)
-
-    ax2 = Axis(fig[2, 1])
-    ax2.xlabel = "SOC (p.u.)"
-    ax2.ylabel = "Error (V)"
-    xlims!(ax2, (0, 1))
-    ylims!(ax2, (-0.1, 0.1))
-    lines!(ax2, s, ocvμ - ocv_meas)
-    vlines!(ax2, vl, color=(:black, 0.5), linestyle=:dash)
-
-    return fig
-end
-
-# fig = plot_gp_ocv(gp)
-# save("figs/gp-ocv1.png", fig, px_per_unit=2)
-
-
-for file in files
-    df = read_basytec(file)
-    focv, cap = calc_pocv(df)
-
-    data = load_profile(df)
-    tt = 0:60:(24*3600)
-    df_train = sample_dataset(data, tt)
-
-    ŝ = df_train.s / 4.9
-    î = df_train.i ./ 4.9
-    v̂ = df_train.v .- 3.7
-    x = GPPPInput(:v, RowVecs([ŝ î]))
-
+    # # GP hyperparams
+    dt = fit_zscore("") # TODO fix this
     θ = (
         kernel=(
             ocv=(σ=0.11650161909960115, l=0.07224889831676123),
@@ -293,24 +155,36 @@ for file in files
         ),
         noise=2.8383204772342602e-5
     )
-    fp = model(θ.kernel)
-    fx = fp(x, θ.noise)
-    gp = posterior(fx, v̂)
+    res = Dict()
+    for file in files
+        # load data
+        id = get_cell_id(file)
+        df = read_basytec(file)
+        data = load_profile(df)
+        df_train = sample_dataset(data, tt)
 
-    fig = plot_gp_ocv(gp, focv, cap, df_train)
-    fig |> display
+        cu = calc_capa_cccv(df) # check-up
+
+        # fit model
+        gp = build_gp(model, θ, df_train, dt)
+
+        res[Symbol(id)] = Dict(:cu => cu, :gp => gp)
+    end
+    return res
 end
 
+function plot_gp_series(res)
+    tt = 0.0:60.0:(3*24*3600.0)
+    tspan = (tt[begin], tt[end])
 
-
-function plot_gp_series(files)
-    fig = Figure(resolution=(1200, 600))
+    fig = Figure(size=(1200, 600))
     ax = [Axis(fig[i, 1]) for i in 1:3]
     ax[1].title = "Measured"
     ax[2].title = "Simulated"
     ax[3].title = "Error"
 
-    tspan = (0.0, 3 * 24 * 3600.0)
+    linkxaxes!(ax...)
+
     xlims!(ax[1], tspan)
     xlims!(ax[2], tspan)
     xlims!(ax[3], tspan)
@@ -328,109 +202,159 @@ function plot_gp_series(files)
     colormap = :dense
     colorrange = (0.6, 1.0)
 
-    for file in files
-        df = read_basytec(file)
+    for file in files # TODO: fix this (files should be an input?)
         id = get_cell_id(file)
-        pocv, cap = calc_pocv(df)
-        soh = cap / 4.9
 
-        data = load_profile(df)
-
-        tt = 0:60:(24*3600)
-        df_train = sample_dataset(data, tt)
-
-        ŝ = df_train.s / 4.9
-        î = df_train.i ./ 4.9
-        v̂ = df_train.v .- 3.7
-        x = GPPPInput(:v, RowVecs([ŝ î]))
-
-        θ = (
-            kernel=(
-                ocv=(σ=0.11650161909960115, l=0.07224889831676123),
-                r=(σ=0.11086743740675992, l=0.0774432873218997)
-            ),
-            noise=2.8383204772342602e-5
-        )
-        fp = model(θ.kernel)
-        fx = fp(x, θ.noise)
-        gp = posterior(fx, v̂)
-
-
-        tt = 0:60:(3*24*3600.0)
+        df_cell = read_basytec(file)
+        data = load_profile(df_cell)
         df_test = sample_dataset(data, tt)
 
-        ŝ = df_test.s / 4.9
-        î = df_test.i ./ 4.9
-        v̄ = df_test.v
-        x = GPPPInput(:v, RowVecs([ŝ î]))
+        cap = res[Symbol(id)][:cu]
+        soh = cap / 4.9
 
-        v = gp(x)
-        vμ = mean(v) .+ 3.7
+        gp = res[Symbol(id)][:gp]
+        vμ, vσ = simulate_gp(gp, df_test)
+
+        v̄ = data.v(df_test.t)
         δv = vμ - v̄
 
-        lines!(ax[1], df_test.t, v̄; color=soh, colorrange, colormap)
-        lines!(ax[2], df_test.t, vμ; color=soh, colorrange, colormap)
-        lines!(ax[3], df_test.t, δv; color=soh, colorrange, colormap)
+        lines!(ax[1], df_test.t, v̄; label=id, color=soh, colorrange, colormap)
+        lines!(ax[2], df_test.t, vμ; label=id, color=soh, colorrange, colormap)
+        lines!(ax[3], df_test.t, δv; label=id, color=soh, colorrange, colormap)
 
         l2 = sum(abs2, δv)
 
         @info id soh l2
-
     end
 
     Colorbar(fig[:, 2]; colorrange, colormap, label="SOH")
-    return fig
+
+    fig
 end
 
-fig = plot_gp_series(files)
+res_gp = fit_gp_series(files)
+fig = plot_gp_series(res_gp)
 save("figs/gp-ecm.svg", fig)
 
 
 ###
-function plot_gp_ocv!(gp, focv, cap, df, ax1, ax2)
-    colormap = :dense
-    colorrange = (0.6, 1.0)
+function plot_gp_ocv!(res, file)
+    id = get_cell_id(file)
+    df = read_basytec(file)
+    data = load_profile(df)
+    tt = 0:60:(24*2600)
+    df_train = sample_dataset(data, tt)
 
-    soh = cap / 4.9
+    focv, cap = calc_pocv(df)
+
+    soh = res[id][:cu] / 4.9
+    gp, dt = res[id][:gp]
+
     s = 0:0.01:1
     s2 = (s .- 0.385) .* soh
     i = zeros(size(s))
     data = RowVecs([s2 i])
     x = GPPPInput(:ocv, data)
+
     ocv_gp = gp(x)
     ocvμ = mean(ocv_gp) .+ 3.7
     ocvσ = sqrt.(var(ocv_gp))
 
     ocv_meas = focv.(s * cap)
 
-    # fig = Figure()
+    fig = Figure()
+    colormap = :dense
+    colorrange = (0.6, 1.0)
 
-    e = [extrema(df.s)...]
+    e = [extrema(df_train.s)...]
     vl = (e ./ 4.9) .+ 0.385
 
-    # ax1 = Axis(fig[1, 1])
-    # ax1.ylabel = "OCV (V)"
-    # xlims!(ax1, (0, 1))
-    # ylims!(ax1, (3.2, 4.2))
+    ax1 = Axis(fig[1, 1])
+    ax1.ylabel = "OCV (V)"
+    xlims!(ax1, (0, 1))
+    ylims!(ax1, (3.2, 4.2))
     lines!(ax1, s, ocvμ)
     lines!(ax1, s, ocv_meas)
     band!(ax1, s, ocvμ - ocvσ, ocvμ + ocvσ)
     vlines!(ax1, vl, color=(:black, 0.5), linestyle=:dash)
 
-    # ax2 = Axis(fig[2, 1])
-    # ax2.xlabel = "SOC (p.u.)"
-    # ax2.ylabel = "Error (V)"
-    # xlims!(ax2, (0, 1))
-    # ylims!(ax2, (-0.1, 0.1))
+    ax2 = Axis(fig[2, 1])
+    ax2.xlabel = "SOC (p.u.)"
+    ax2.ylabel = "Error (V)"
+    xlims!(ax2, (0, 1))
+    ylims!(ax2, (-0.1, 0.1))
     lines!(ax2, s, ocvμ - ocv_meas; color=soh, colorrange, colormap)
     vlines!(ax2, vl, color=(:black, 0.5), linestyle=:dash)
 
-    # return fig
+    return fig
 end
 
+function plot_gp_ecm(res, file)
+    id = get_cell_id(file)
+    df = read_basytec(file)
+    data = load_profile(df)
+    tt = 0:60:(24*2600)
+    df_train = sample_dataset(data, tt)
+
+    focv, cap = calc_pocv(df)
+
+    soh = res[Symbol(id)][:cu] / 4.9
+    gp, dt = res[Symbol(id)][:gp]
+
+    e = [extrema(df_train.s)...]
+    vl = (e ./ 4.9) .+ 0.385
+
+    fig = Figure(size=(1000, 500))
+
+    # OCV
+    s = 0:0.01:1
+    s2 = (s .- 0.385) .* soh
+    i = zeros(size(s))
+    data = RowVecs([s2 i])
+    x = GPPPInput(:ocv, data)
+
+    ocv_gp = gp(x)
+    ocvμ = mean(ocv_gp) .+ 3.7
+    ocvσ = sqrt.(var(ocv_gp))
+
+    ocv_meas = focv.(s * cap)
+
+    colormap = :dense
+    colorrange = (0.6, 1.0)
+
+    ax1 = Axis(fig[1, 1])
+    ax1.ylabel = "OCV (V)"
+    xlims!(ax1, (0, 1))
+    ylims!(ax1, (3.2, 4.2))
+    lines!(ax1, s, ocvμ)
+    lines!(ax1, s, ocv_meas)
+    band!(ax1, s, ocvμ - ocvσ, ocvμ + ocvσ)
+    vlines!(ax1, vl, color=(:black, 0.5), linestyle=:dash)
+
+    # R
+    x = GPPPInput(:r, data)
+
+    r = gp(x)
+    rμ = mean(r) / 4.9 * 1e3
+    rσ = sqrt.(var(r)) / 4.9 * 1e3
+
+    ax2 = Axis(fig[2, 1])
+    ax2.xlabel = "SOC (p.u.)"
+    ax2.ylabel = "R (mΩ)"
+    xlims!(ax2, (0, 1))
+    # ylims!(ax2, (-0.1, 0.1))
+    lines!(ax2, s, rμ; color=soh, colorrange, colormap)
+    band!(ax2, s, rμ - rσ, rμ + rσ)
+    vlines!(ax2, vl, color=(:black, 0.5), linestyle=:dash)
+
+    return fig
+end
+
+fig = plot_gp_ecm(res_gp, files[end])
+fig |> save("figs/ecm-params-fresh-cell.svg")
 
 function plot_gp_ocv_series(files)
-    fig = Figure(resolution=(1000, 600))
+    fig = Figure(size=(1000, 600))
 
     g1 = GridLayout(fig[1, 1])
     ci = CartesianIndices((1:2, 1:5))
@@ -499,20 +423,229 @@ fig = plot_gp_ocv_series(files)
 save("figs/multi-ocv.svg", fig)
 
 
-begin
-    file = "data/check-ups/2098LG_INR21700-M50L_SammyLGL13818NewFullCU.txt"
-    df = read_basytec(file)
 
-    fig = Figure(resolution=(1200, 400))
-    ax = Axis(fig[1, 1])
-    n = nrow(df)
-    lines!(ax, df[1:60:n, "Time[h]"], df[1:60:n, "U[V]"])
 
-    xlims!(ax, (df[begin, "Time[h]"], df[end, "Time[h]"]))
-    ax.xlabel = "Time (h)"
-    ax.ylabel = "Voltage (V)"
-    fig
+file = "data/check-ups/2098LG_INR21700-M50L_SammyLGL13818NewFullCU.txt"
+fig = plot_checkup_profile(file)
+fig |> save("figs/checkup-2.svg")
+
+##
+
+
+# function fit_gp_rc_series(files)
+#     res = Dict()
+
+#     # θ = fit_gp_hyperparams(model, θ0, df)
+#     θ = (
+#         kernel=(
+#             ocv=(σ=0.11650161909960115, l=0.07224889831676123),
+#             r=(σ=0.11086743740675992, l=0.0774432873218997)
+#         ),
+#         noise=2.8383204772342602e-5
+#     )
+#     tt = 0:60:(24*3600)
+
+#     for file in files
+#         df = read_basytec(file)
+#         id = get_cell_id(file)
+#         # pocv, cap = calc_pocv(df)
+
+#         cu = calc_capa_cccv(df) # check-up
+
+#         data = load_profile(df)
+#         df_train = sample_dataset(data, tt)
+
+#         s = df_train.s / 4.9
+#         i = df_train.i ./ 4.9
+#         v = df_train.v .- 3.7
+#         x = GPPPInput(:v, RowVecs([s i]))
+
+#         fi = t -> data.i(t)
+#         @mtkbuild rc = RC(; fi)
+#         tspan = (tt[begin], tt[end])
+#         ode = ODEProblem(rc, [rc.v1 => 0.0], tspan, [])
+#         p = fit_gp_rc(rc, ode, model, θ, df_train)
+#         ode = remake(ode; p)
+#         sol = solve(ode, saveat=60)
+#         vrc = sol[rc.v1]
+
+#         fp = model(θ.kernel)
+#         fx = fp(x, θ.noise)
+#         gp = posterior(fx, v - vrc)
+
+#         res[id] = Dict(:cu => cu, :gp => gp, :ode => ode, :model => rc)
+#     end
+#     return res
+# end
+function build_loss_rc2(mtk_model, prob, fx, v, t)
+    return loss_rc(x) = begin
+        @unpack u0, p = x
+        newprob = remake(prob; u0, p)
+        sol = solve(newprob; saveat=t) # TODO: rate as param
+        vrc = sol[mtk_model.v1]
+        -logpdf(fx, v - vrc)
+    end
+end
+function fit_gp_rc_series(files)
+    res = Dict()
+
+    # θ = fit_gp_hyperparams(model, θ0, df)
+    θ = (
+        kernel=(
+            ocv=(σ=0.11650161909960115, l=0.07224889831676123),
+            r=(σ=0.11086743740675992, l=0.0774432873218997)
+        ),
+        noise=2.8383204772342602e-5
+    )
+    tt = 0:60:(24*3600)
+
+    for file in files
+        df = read_basytec(file)
+        id = get_cell_id(file)
+        # pocv, cap = calc_pocv(df)
+
+        cu = calc_capa_cccv(df) # check-up
+
+        data = load_profile(df)
+        df_train = sample_dataset(data, tt)
+
+        s = df_train.s / 4.9
+        i = df_train.i ./ 4.9
+        v = df_train.v .- 3.7
+        x = GPPPInput(:v, RowVecs([s i]))
+        fp = model(θ.kernel)
+        fx = fp(x, θ.noise)
+
+        fi = t -> data.i(t)
+        @mtkbuild rc = RC(; fi)
+        tspan = (tt[begin], tt[end])
+        ode = ODEProblem(rc, [rc.v1 => 0.0], tspan, [])
+
+        loss = build_loss_rc2(rc, ode, fx, v, df_train.t)
+        p0 = ComponentArray((;
+            u0=ode.u0,
+            p=ode.p
+        ))
+
+        adtype = AutoForwardDiff()
+        f = OptimizationFunction((u, p) -> loss(u), adtype)
+        prob = OptimizationProblem(f, p0)
+
+        sol = solve(prob, LBFGS(); show_trace=true)
+
+        ode = remake(ode; sol.u...)
+        sol = solve(ode, saveat=60)
+        vrc = sol[rc.v1]
+
+        fp = model(θ.kernel)
+        fx = fp(x, θ.noise)
+        gp = posterior(fx, v - vrc)
+
+        res[id] = Dict(:cu => cu, :gp => gp, :ode => ode, :model => rc)
+    end
+    return res
 end
 
-save("figs/checkup.svg", fig)
+res = fit_gp_rc_series(files)
 
+function plot_gp_rc_series(files)
+    fig = Figure(size=(1200, 600))
+    ax = [Axis(fig[i, 1]) for i in 1:3]
+    ax[1].title = "Measured"
+    ax[2].title = "Simulated"
+    ax[3].title = "Error"
+
+    tspan = (0.0, 3 * 24 * 3600.0)
+    xlims!(ax[1], tspan)
+    xlims!(ax[2], tspan)
+    xlims!(ax[3], tspan)
+    ylims!(ax[1], (3.3, 4.1))
+    ylims!(ax[2], (3.3, 4.1))
+    ylims!(ax[3], (-0.15, 0.15))
+    ax[1].ylabel = "Voltage (V)"
+    ax[2].ylabel = "Voltage (V)"
+    ax[3].ylabel = "Voltage (V)"
+    ax[3].xlabel = "Time (s)"
+
+    hidexdecorations!(ax[1])
+    hidexdecorations!(ax[2])
+
+    colormap = :dense
+    colorrange = (0.6, 1.0)
+
+    for file in files
+        df = read_basytec(file)
+        id = get_cell_id(file)
+        pocv, cap = calc_pocv(df)
+        soh = cap / 4.9
+
+        data = load_profile(df)
+
+        tt = 0:60:(3*24*3600)
+        df_test = sample_dataset(data, tt)
+
+        # rc
+        ode = res[id][:ode]
+        rc = res[id][:model]
+
+        tspan = (tt[begin], tt[end])
+        ode = remake(ode; tspan)
+        sol = solve(ode, saveat=df_test.t)
+        vrc = sol[rc.v1]
+
+        # gp
+        ŝ = df_test.s / 4.9
+        î = df_test.i ./ 4.9
+        v̄ = df_test.v
+        x = GPPPInput(:v, RowVecs([ŝ î]))
+
+        gp = res[id][:gp]
+
+        # results
+        v = gp(x)
+        vμ = mean(v) + vrc .+ 3.7
+        δv = vμ - v̄
+
+        lines!(ax[1], df_test.t, v̄; color=soh, colorrange, colormap)
+        lines!(ax[2], df_test.t, vμ; color=soh, colorrange, colormap)
+        lines!(ax[3], df_test.t, δv; color=soh, colorrange, colormap)
+
+        l2 = sum(abs2, δv)
+
+        @info id soh l2
+
+    end
+
+    Colorbar(fig[:, 2]; colorrange, colormap, label="SOH")
+    return fig
+end
+
+res_rc = fit_gp_rc_series(files)
+fig = plot_gp_rc_series(files)
+
+fig |> save("figs/gp-rc-ecm.svg")
+
+###
+begin
+    fig = Figure()
+    ax = [Axis(fig[i, 1]) for i in 1:3]
+    colormap = :dense
+    colorrange = (0.6, 1.0)
+
+    for file in files
+
+        df_cell = read_basytec(file)
+        data = load_profile(df_cell)
+        cap = calc_capa_cccv(df_cell)
+        soh = cap / 4.9
+        tt = 0.0:60.0:(24*3600.0)
+
+        df = sample_dataset(data, tt)
+
+        lines!(ax[1], df.t, df.v; color=soh, colorrange, colormap)
+        lines!(ax[2], df.t, df.i; color=soh, colorrange, colormap)
+        lines!(ax[3], df.t, df.s / cap; color=soh, colorrange, colormap)
+    end
+    Colorbar(fig[:, 2]; colorrange, colormap, label="SOH")
+    fig
+end
