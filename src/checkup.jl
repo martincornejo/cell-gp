e1bd4cd009a9a3c07143ff7fb19c8fa03718ab37
Martin Cornejo <martin.cornejo@tum.de>
2024-01-25 16:37:27 +0100

    Plots for check-up analysis

--- src/checkup.jl
+++ src/checkup.jl
@@ -105,3 +105,86 @@ function sample_dataset(data, tt)
         :T => data.T(tt)
     )
 end
+
+
+## plots
+function plot_checkup_profile(file)
+    df = read_basytec(file)
+
+    # fig = Figure(size=(1200, 400))
+    # fig = Figure(size=(900, 300))
+    fig = Figure(size=(600, 200), fontsize=11, figure_padding=5)
+    gl = GridLayout(fig[1,1])
+    # fig = Figure(size=(515, 172), fontsize=11)
+    ax1 = Axis(gl[1, 1])
+    ax2 = Axis(gl[2, 1])
+    n = nrow(df)
+    lines!(ax1, df[1:60:n, "Time[h]"], df[1:60:n, "U[V]"], color=Cycled(1))
+    lines!(ax2, df[1:60:n, "Time[h]"], df[1:60:n, "I[A]"], color=Cycled(2))
+    xlims!(ax1, (df[begin, "Time[h]"], df[end, "Time[h]"]))
+    xlims!(ax2, (df[begin, "Time[h]"], df[end, "Time[h]"]))
+    ax1.ylabel = "Voltage (V)"
+    ax2.ylabel = "Current (A)"
+    ax2.xlabel = "Time (h)"
+    hidexdecorations!(ax1, grid=false, ticks=false)
+    rowgap!(gl, 5)
+    fig
+end
+
+function plot_ocvs(files)
+    s = 0:0.001:1.0
+    ylabel = "Voltage in V"
+    xlabel = "SOC in p.u."
+    colormap = :dense
+    colorrange = (0.6, 1.0)
+
+    fig1 = Figure(size=(800, 400))
+    ax1 = Axis(fig1[1, 1]; xlabel, ylabel, title="Charge pOCV")
+    ax2 = Axis(fig1[1, 2]; xlabel, title="Discharge pOCV")
+    linkyaxes!(ax1, ax2)
+    hideydecorations!(ax2, ticks=false, grid=false)
+
+    fig2 = Figure(size=(900, 400))
+    ax3 = Axis(fig2[1, 1]; xlabel, ylabel, title="Mean pOCV")
+    ax4 = Axis(fig2[1, 2]; xlabel, title="OCV Hystheresis")
+
+    fig3 = Figure(size=(900, 400))
+    ax5 = Axis(fig3[1, 1]; xlabel, ylabel, title="Mean pOCV")
+    ax6 = Axis(fig3[1, 2]; xlabel, title="Deviation")
+
+    focv = fresh_focv()
+
+    for file in files
+        df = read_basytec(file)
+
+        # charge ocv
+        ocv_c, cap_c = calc_cocv(df)
+        c = s .* cap_c
+        lines!(ax1, s, ocv_c(c); color=cap_c / 4.9, colorrange, colormap)
+
+        # discharge ocv
+        ocv_d, cap_d = calc_docv(df)
+        c = s .* cap_d
+        lines!(ax2, s, ocv_d(c); color=cap_d / 4.9, colorrange, colormap)
+
+        # mean ocv
+        pocv, cap = calc_pocv(df)
+        c = s .* cap
+        lines!(ax3, s, pocv(c); color=cap / 4.9, colorrange, colormap)
+        lines!(ax5, s, pocv(c); color=cap / 4.9, colorrange, colormap)
+
+        # hyst
+        lines!(ax4, s, pocv(c) - ocv_c(c); color=cap / 4.9, colorrange, colormap)
+
+        # degradation
+        if !occursin("LGL13818", file)
+            lines!(ax6, s, pocv(c) - focv(s); color=cap / 4.9, colorrange, colormap)
+        end
+    end
+
+    Colorbar(fig1[:, 3]; colorrange, colormap, label="SOH")
+    Colorbar(fig2[:, 3]; colorrange, colormap, label="SOH")
+    Colorbar(fig3[:, 3]; colorrange, colormap, label="SOH")
+    return fig1, fig2, fig3
+end
+
