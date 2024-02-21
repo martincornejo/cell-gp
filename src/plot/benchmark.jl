654958f0dac841d6611b4fc40a6b50c4d7fc2e2a
Martin Cornejo <martin.cornejo@tum.de>
2024-02-21 23:46:38 +0100

    Move plotting functions to dedicated files

---
+++ src/plot/benchmark.jl
@@ -0,0 +1,105 @@
+
+function plot_ocv_fit(ecm, gpm, df, id; correct_soc=true, plot_gp=true)
+    fig = Figure()
+    ax1 = Axis(fig[1, 1])
+    ax2 = Axis(fig[1, 1], yaxisposition=:right)
+    ax1.ylabel = "Voltage (V)"
+    ax1.xlabel = "SOC (Ah)"
+    ylims!(ax1, 3.3, 4.3)
+    ylims!(ax2, 0, .5e3)
+
+    ## ECM
+    # function ocv(ecm, df)
+    pocv = calc_pocv(df)
+    focv = fresh_focv()
+    s = 0:0.001:1
+
+    cap_real = calc_capa_cccv(df)
+    cap_sim = ecm.ode.p[1]
+
+    soc0 = 0.385 * calc_capa_cc(df)
+    soc_real = soc0 / cap_real
+    soc_sim = ecm.ode.u0[1]
+    Δsoc = correct_soc ? soc_sim - soc_real : 0.0
+
+    # real
+    c1 = s .* cap_real
+    ocv1 = pocv(s)
+    lines!(ax1, c1, ocv1; label="pOCV", color=:black)
+
+    # estimated
+    c2 = (s .- Δsoc) .* cap_sim
+    ocv2 = focv(s)
+    lines!(ax1, c2, ocv2; label="ECM")
+
+    soh = round(cap_real / 4.9 * 100; digits=2)
+    ax1.title = "ID:$id SOH: $soh %"
+
+    ## GP
+    if plot_gp
+        @unpack gp, dt = gpm
+        ŝ = StatsBase.transform(dt.s, (s * cap_real) .- soc0)
+        x = GPPPInput(:ocv, RowVecs([ŝ zero.(ŝ)]))
+        ocv3 = gp(x)
+        μ = StatsBase.reconstruct(dt.v, mean(ocv3))
+        σ = StatsBase.reconstruct(dt.σ, sqrt.(var(ocv3)))
+        lines!(ax1, c1, μ; label="GP", color=Cycled(2))
+        band!(ax1, c1, μ - σ, μ + σ; label="GP", color=(Makie.wong_colors()[2], 0.3))
+    end
+
+    ## SOC
+    tt = 0:60:(24*3600)
+    profile = load_profile(df)
+    df_train = sample_dataset(profile, tt)
+
+    hist!(ax2, df_train.s .+ soc0, color=(:gray, 0.3), label="SOC")
+    hideydecorations!(ax2)
+    axislegend(ax1; position=:rb, merge=true)
+    axislegend(ax2; position=:lt)
+
+    smin, smax = extrema(df_train.s) .+ soc0
+    vlines!(ax1, [smin, smax], color=:gray, linestyle=:dashdot)
+
+    linkxaxes!(ax1, ax2)
+    fig
+end
+
+function plot_rint_fit(df)
+    fig = Figure()
+    ax = Axis(fig[1, 1], xlabel="Measured pulse resistance (mΩ)", ylabel="Estimated pulse resistance (mΩ)")
+    ylims!(ax, 18, 75)
+    xlims!(ax, 18, 75)
+    ablines!(ax, 0, 1, linestyle=:dashdot, color=:black)
+
+    # ECM
+    scatter!(ax, df.r_cu, df.r_ecm, label="ECM")
+
+    # ECM-GP
+    μ = Measurements.value.(df.r_gp)
+    σ = Measurements.uncertainty.(df.r_gp)
+    scatter!(ax, df.r_cu, μ, label="ECM-GP")
+    errorbars!(ax, df.r_cu, μ, σ; whiskerwidth=5, color=Makie.wong_colors()[2])
+
+    axislegend(ax; position=:rb)
+    fig
+end
+
+
+function plot_soh_fit(df)
+    fig = Figure()
+    ax = Axis(fig[1, 1], xlabel="Check-up SOH (%)", ylabel="Estimated SOH (%)")
+    ablines!(ax, 0, 1, linestyle=:dashdot, color=:black)
+
+    # ECM
+    scatter!(ax, df.soh_cu, df.soh_ecm, label="ECM")
+
+    # ECM-GP
+    μ = Measurements.value.(df.soh_ocv_gp)
+    σ = Measurements.uncertainty.(df.soh_ocv_gp)
+    scatter!(ax, df.soh_cu, μ, label="ECM-GP")
+    errorbars!(ax, df.soh_cu, μ, σ; whiskerwidth=5, color=Makie.wong_colors()[2])
+    # scatter!(ax, df.soh_cu, df.soh_ocv_ecm, label="ECM-OCV")
+
+    axislegend(ax; position=:rb)
+    fig
+end
---
+++ src/plot/dataset.jl
@@ -0,0 +1,73 @@
+function plot_checkup_profile(df)
+    # fig = Figure(size=(600, 200), fontsize=11, figure_padding=5)
+    fig = Figure(size=(1000, 300))
+    gl = GridLayout(fig[1, 1])
+    ax1 = Axis(gl[1, 1]; ylabel="Voltage (V)")
+    ax2 = Axis(gl[2, 1]; ylabel="Current (A)", xlabel="Time (h)")
+
+    lines!(ax1, df[:, "Time[h]"], df[:, "U[V]"], color=Cycled(1))
+    lines!(ax2, df[:, "Time[h]"], df[:, "I[A]"], color=Cycled(2))
+    xlims!(ax1, (df[begin, "Time[h]"], df[end, "Time[h]"]))
+    xlims!(ax2, (df[begin, "Time[h]"], df[end, "Time[h]"]))
+
+    idx = [findfirst(df[:, "Line"] .== line) for line in (25, 35, 36, 47)]
+    vlines!(ax1, df[idx, "Time[h]"], color=:gray, linestyle=:dashdot)
+    vlines!(ax2, df[idx, "Time[h]"], color=:gray, linestyle=:dashdot)
+
+    hidexdecorations!(ax1, grid=false, ticks=false)
+    rowgap!(gl, 5)
+    fig
+end
+
+function plot_ocvs(data)
+    # fig = Figure(size=(350, 400), fontsize=11, figure_padding=7)
+    fig = Figure()
+    gl = GridLayout(fig[1, 1])
+    colormap = :dense
+    colorrange = (0.6, 1.0)
+
+    Colorbar(gl[1, 1]; colorrange, colormap, label="SOH", vertical=false) #, flipaxis=false)
+    ax1 = Axis(gl[2, 1]; ylabel="OCV (V)") # mean pOCV
+    ax2 = Axis(gl[3, 1]; xlabel="SOC", ylabel="δV (mV)")
+    rowgap!(gl, 6)
+
+    focv = fresh_focv()
+    s = 0:0.001:1.0
+
+    for (id, df) in data
+        soh = calc_capa_cccv(df) / 4.9
+
+        # mean ocv
+        pocv = calc_pocv(df)
+        lines!(ax1, s, pocv(s); color=soh, colorrange, colormap)
+
+        # degradation
+        if id != :LGL13818
+            δv = (pocv(s) - focv(s)) * 1e3 # mV
+            lines!(ax2, s, δv; color=soh, colorrange, colormap)
+        end
+    end
+
+    linkxaxes!(ax1, ax2)
+    hidexdecorations!(ax1, grid=false, ticks=false)
+    return fig
+end
+
+function plot_rints(data; timestep=9.99)
+    soc = 0.9:-0.1:0.1
+    colormap = :dense
+    colorrange = (0.6, 1.0)
+
+    fig = Figure(size=(359, 350), fontsize=11)
+    ax = Axis(fig[1, 1]; xlabel="SOC", ylabel="Resistance (mΩ)")
+    Colorbar(fig[1, 2]; colorrange, colormap, label="SOH")
+
+    for (id, df) in data
+        soh = calc_capa_cccv(df) / 4.9
+        r = calc_rint(df; timestep) * 1e3 # mΩ
+
+        scatter!(ax, soc, r; color=soh, colorrange, colormap)
+    end
+
+    return fig
+end
