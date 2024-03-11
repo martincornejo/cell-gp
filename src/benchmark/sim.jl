89639c167a3e33a2f0914a9ec39a224c5d876227
Martin Cornejo <martin.cornejo@tum.de>
2024-03-11 14:33:42 +0100

    Update dataset time horizon

--- src/benchmark/sim.jl
+++ src/benchmark/sim.jl
@@ -1,4 +1,7 @@
-function simulation_error(ecm_model, gp_model, df, tt)
+function simulation_error(ecm_model, gp_model, df)
+    # time-span
+    tt = 0:60:(6.9*24*3600)
+
     # real voltage
     profile = load_profile(df)
     df_test = sample_dataset(profile, tt)
@@ -18,35 +21,35 @@ function simulation_error(ecm_model, gp_model, df, tt)
     return (; l2_ecm, l2_gp)
 end
 
-function benchmark_simulation(ecms, gpms, data, tt)
+function benchmark_simulation(ecms, gpms, data)
     sohs = [calc_capa_cccv(df) / 4.9 for df in values(data)]
     ids = collect(keys(data))[sortperm(sohs)] # sort cell-id by soh
 
-    sim = ids .|> id -> simulation_error(ecms[id], gpms[id], data[id], tt)
+    sim = ids .|> id -> simulation_error(ecms[id], gpms[id], data[id])
     l2_ecm = sim .|> first
     l2_gp = sim .|> last
     DataFrame(; id=ids, l2_ecm, l2_gp)
 end
 
 function plot_sim(ecm_models, gp_models, data)
-    fig = Figure(size=(1200, 600))
+    fig = Figure(size=(1200, 500))
     ax = [Axis(fig[i, 1]) for i in 1:3]
     ax[1].title = "Measured"
     ax[2].title = "Error ECM"
     ax[3].title = "Error GP-ECM"
 
-    tt = 0:60:(3*24*3600)
+    tt = 0:60:(6.9*24*3600)
     tspan = (tt[begin], tt[end])
     tlims = tspan ./ 3600
     xlims!(ax[1], tlims)
     xlims!(ax[2], tlims)
     xlims!(ax[3], tlims)
-    ylims!(ax[1], (3.3, 4.1))
+    ylims!(ax[1], (3.2, 4.2))
     ylims!(ax[2], (-75, 75))
     ylims!(ax[3], (-75, 75))
-    ax[1].xticks = 0:12:(3*24)
-    ax[2].xticks = 0:12:(3*24)
-    ax[3].xticks = 0:12:(3*24)
+    ax[1].xticks = 0:12:(7*24)
+    ax[2].xticks = 0:12:(7*24)
+    ax[3].xticks = 0:12:(7*24)
     ax[1].ylabel = "Voltage (V)"
     ax[2].ylabel = "Voltage (mV)"
     ax[3].ylabel = "Voltage (mV)"
