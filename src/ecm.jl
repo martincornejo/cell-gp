36d165ad8460d38aa5c4a02e5b0456a3f296fc10
Martin Cornejo <martin.cornejo@tum.de>
2024-06-01 20:55:40 +0200

    Update ECM fit: fix initial SOC

--- src/ecm.jl
+++ src/ecm.jl
@@ -6,10 +6,10 @@
     end
     @parameters begin
         Q = 4.8
-        R0 = 0.5e-3
-        R1 = 0.5e-3
+        R0 = 1e-3
+        R1 = 1e-3
         τ1 = 60
-        R2 = 0.5e-3
+        R2 = 1e-3
         τ2 = 3600
     end
     @variables begin
@@ -40,7 +40,9 @@ end
 ## fit model
 function build_loss_function(prob, model, df)
     loss(x) = begin
-        @unpack u0, p = x
+        (; u, p) = x
+        u0 = vcat(prob.u0[1], u) # add inital soc
+
         newprob = remake(prob; u0, p)
         sol = solve(newprob, Tsit5(); saveat=df.t)
         v = sol[model.v]
@@ -48,21 +50,21 @@ function build_loss_function(prob, model, df)
     end
 end
 
-function fit_ecm(df, tt, focv) # input only data instead?
+function fit_ecm(df, tt, focv)
     # load dataset
     profile = load_profile(df)
     df_train = sample_dataset(profile, tt)
+    soc = initial_soc(df)
 
     # build model
     fi = t -> profile.i(t)
     @mtkbuild ecm = ECM(; focv, fi)
-
     tspan = (tt[begin], tt[end])
-    ode = ODEProblem(ecm, [ecm.soc => 0.5], tspan, [])
+    ode = ODEProblem(ecm, [ecm.soc => soc], tspan, [])
 
     # build optimization problem
-    p0 = ComponentArray((;
-        u0=ode.u0,
+    p0 = ComponentArray((; # param initial guess
+        u=zeros(2),
         p=ode.p
     ))
     adtype = Optimization.AutoForwardDiff() # auto-diff framework
@@ -72,26 +74,31 @@ function fit_ecm(df, tt, focv) # input only data instead?
 
     # optimize with multiple initial values, select best fit
     solutions = []
-    for Q in (4.8, 4.4, 4.0, 3.6)
+    for Q in 3.6:0.4:4.8
+        # update initial guess of Q
         p0.p[1] = Q
-        opt = remake(opt; p=p0)
+        opt = remake(opt; u0=p0)
 
-        sol = solve(opt, LBFGS(); reltol=1e-4)
+        # solve
+        alg = LBFGS(linesearch=BackTracking())
+        sol = solve(opt, alg; reltol=1e-4)
         if sol.retcode == ReturnCode.Success
             push!(solutions, sol)
         end
     end
     opt_sol = argmin(sol -> sol.objective, solutions)
 
-    @unpack u0, p = opt_sol.u
-    ode = remake(ode; u0, p, tspan)
+    # build parametrized model
+    (; u, p) = opt_sol.u
+    u0 = vcat(ode.u0[1], u) # add inital soc
+    ode = remake(ode; p, u0)
 
     return (; ecm, ode)
 end
 
 
 function fit_ecm_series(data)
-    tt = 0:(3*24*3600.0)
+    tt = 0:(3*24*3600.0) # training time window
     focv = fresh_focv()
     res = Dict()
     Threads.@threads for id in collect(eachindex(data))
