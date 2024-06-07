719cb915f8d011d99ca3ed348b51cad48a6f0481
Martin Cornejo <martin.cornejo@tum.de>
2024-06-07 17:41:16 +0200

    Update OCV fit plot

--- src/plot/benchmark.jl
+++ src/plot/benchmark.jl
@@ -1,8 +1,9 @@
 
-function plot_ocv_fit(ecms, gpms, data, ids; correct_soc=true)
+function plot_ocv_fit(ecms, gpms, data)
     fig = Figure(size=(512, 700), fontsize=10, figure_padding=5)
     gl = [GridLayout(fig[ci[2], ci[1]]) for ci in CartesianIndices((1:2, 1:5))]
 
+    ids = sort_cell_ids(data)
     for (i, id) in enumerate(ids)
         df = data[id]
         ecm = ecms[id]
@@ -17,46 +18,43 @@ function plot_ocv_fit(ecms, gpms, data, ids; correct_soc=true)
         xlims!(ax1, 0, 5)
         xlims!(ax2, 0, 5)
 
-        ## ECM
-        # function ocv(ecm, df)
+        # parameters
         pocv = calc_pocv(df)
         focv = fresh_focv()
         s = 0:0.001:1
 
-        cap_real = calc_capa_cccv(df)
-        cap_sim = ecm.ode.p[1]
-
+        cap_cu = calc_capa_cccv(df)
+        cap_ecm = ecm.ode.p[1]
         soc0 = initial_soc(df)
-        soc0_sim = ecm.ode.u0[1]
-        Δsoc = correct_soc ? soc0_sim - soc0 : 0.0
 
-        # real
-        c1 = s .* cap_real
-        ocv1 = pocv(s)
-        ln1 = lines!(ax1, c1, ocv1; label="pOCV", color=:black)
+        # ECM
+        q_ecm = s .* cap_ecm
+        ocv_ecm = focv(s)
+        ln2 = lines!(ax1, q_ecm, ocv_ecm; label="ECM")
 
-        # estimated
-        c2 = (s .- Δsoc) .* cap_sim
-        ocv2 = focv(s)
-        ln2 = lines!(ax1, c2, ocv2; label="ECM")
-
-        ## GP
-        @unpack gp, dt = gpm
-        ŝ = StatsBase.transform(dt.s, (s .- soc0) * cap_real)
+        # GP-ECM
+        (; gp, dt) = gpm
+        q_gp = 0:0.005:5
+        ŝ = StatsBase.transform(dt.s, q_gp .- soc0 * cap_cu)
         x = GPPPInput(:ocv, RowVecs([ŝ zero.(ŝ)]))
-        ocv3 = gp(x)
-        μ = StatsBase.reconstruct(dt.v, mean(ocv3))
-        σ = StatsBase.reconstruct(dt.σ, sqrt.(var(ocv3)))
-        ln3 = lines!(ax1, c1, μ; label="GP-ECM", color=Cycled(2))
-        bnd = band!(ax1, c1, μ - 2σ, μ + 2σ; label="GP-ECM", color=(Makie.wong_colors()[2], 0.3))
+        ocv_gp = gp(x)
+        μ = StatsBase.reconstruct(dt.v, mean(ocv_gp))
+        σ = StatsBase.reconstruct(dt.σ, sqrt.(var(ocv_gp)))
+        ln3 = lines!(ax1, q_gp, μ; label="GP-ECM", color=Cycled(2))
+        bnd = band!(ax1, q_gp, μ - 2σ, μ + 2σ; label="GP-ECM", color=(Makie.wong_colors()[2], 0.3))
 
-        ## SOC
+        # Measured pOCV
+        q_cu = s .* cap_cu
+        ocv_cu = pocv(s)
+        ln1 = lines!(ax1, q_cu, ocv_cu; label="pOCV", color=:black, linestyle=:dot)
+
+        # SOC distribution
         tt = 0:60:(3*24*3600)
         profile = load_profile(df)
         df_train = sample_dataset(profile, tt)
 
-        hst = hist!(ax2, df_train.s .+ soc0 * cap_real, color=(:gray, 0.3), label="SOC")
-        smin, smax = extrema(df_train.s) .+ soc0 * cap_real
+        hst = hist!(ax2, df_train.s .+ soc0 * cap_cu, color=(:gray, 0.3), label="SOC")
+        smin, smax = extrema(df_train.s) .+ soc0 * cap_cu
         vlines!(ax1, [smin, smax], color=:gray, linestyle=:dashdot)
         hidedecorations!(ax2)
         hidespines!(ax2)
@@ -80,7 +78,7 @@ function plot_ocv_fit(ecms, gpms, data, ids; correct_soc=true)
         end
 
         # text box
-        soh = round(cap_real / 4.85 * 100; digits=1)
+        soh = round(cap_cu / 4.85 * 100; digits=1)
         text = "Cell $i \nSOH: $soh %"
         poly!(ax1, Rect(0.1, 3.95, 1.5, 0.29), color=:white, strokecolor=:black, strokewidth=1)
         text!(ax1, 0.15, 4.2; text, font=:bold, align=(:left, :top))
