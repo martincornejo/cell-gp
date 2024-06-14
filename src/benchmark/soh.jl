22d57f45e622f10adcd976ffb428db492bc47ae7
Martin Cornejo <martin.cornejo@tum.de>
2024-06-14 20:20:43 +0200

    Update SOH benchmark

--- src/benchmark/soh.jl
+++ src/benchmark/soh.jl
@@ -1,65 +1,75 @@
 
-function ocv_capa_gp(gp_model, df; v=(3.6, 3.8))
-    profile = load_profile(df)
-    tt = 0:60:(24*2600)
-    df_train = sample_dataset(profile, tt)
-
-    cap = calc_capa_cccv(df)
-    cmin, cmax = extrema(df_train.s) # charge
-    crange = cmin:0.001:cmax
-    soc0 = initial_soc(df)
-    s = crange .+ soc0 * cap
-    v1, v2 = v
-
-    (; gp, dt) = gp_model
-    ŝ = StatsBase.transform(dt.s, crange)
-    i = zero.(ŝ)
-    x = GPPPInput(:ocv, RowVecs([ŝ i]))
-    ocv_gp = gp(x)
-    μ = StatsBase.reconstruct(dt.v, mean(ocv_gp))
-    σ = StatsBase.reconstruct(dt.σ, sqrt.(var(ocv_gp)))
-
-    c1μ = s[findfirst(>=(v1), μ)]
-    c1σ = c1μ - s[findfirst(>=(v1), μ + σ)]
-    c1 = c1μ ± c1σ
-
-    c2μ = s[findfirst(>=(v2), μ)]
-    c2σ = s[findfirst(>=(v2), μ - σ)] - c2μ
-    c2 = c2μ ± c2σ
-
-    return c2 - c1
-end
-
-function ocv_capa_ecm(ecm, df, focv; v=(3.6, 3.8))
-    profile = load_profile(df)
-    tt = 0:60:(24*2600)
-    df_train = sample_dataset(profile, tt)
-
-    cap = calc_capa_cccv(df)
-    soc0 = initial_soc(df)
-    cmin, cmax = extrema(df_train.s) # charge
-    crange = (cmin:0.001:cmax) .+ soc0 * cap
-    srange = crange ./ cap
-
-    cap_ecm = ecm.ode.p[1]
-    ocv = focv(srange)
-    f = ConstantInterpolation(srange .* cap_ecm, ocv)
-    v1, v2 = v
-    return f(v2) - f(v1)
-end
-
-function benchmark_soh(ecms, gpms, data; v=(3.6, 3.8))
-    v1, v2 = v
+function benchmark_soh(ecms, gpms, data; v=(3.6, 3.9))
     focv = fresh_focv(data)
-    invf = ConstantInterpolation((0:0.01:1) .* 4.9, focv(0:0.01:1))
-    soh0 = invf(v2) - invf(v1)
+    tt = 0:60:(24*2600)
+    s = 0.2:0.001:0.8
 
     ids = sort_cell_ids(data)
-    soh_cu = ids .|> id -> calc_capa_cccv(data[id]) / 4.9
-    soh_ocv_gp = ids .|> id -> ocv_capa_gp(gpms[id], data[id]; v) / soh0
-    soh_ocv_ecm = ids .|> id -> ocv_capa_ecm(ecms[id], data[id], focv; v) / soh0
-    soh_ecm = ids .|> id -> ecms[id].ode.p[1] / 4.9
+    res = map(ids) do id
+        df = data[id]
+        ecm = ecms[id]
+        gpm = gpms[id]
 
-    return DataFrame(; id=ids, soh_cu, soh_ocv_gp, soh_ocv_ecm, soh_ecm)
+        profile = load_profile(df)
+        df_train = sample_dataset(profile, tt)
+
+        v1, v2 = v
+        cap = calc_capa_cccv(df)
+        soc0 = initial_soc(df)
+        q = (s .- soc0) .* cap
+
+        # measured
+        pocv = calc_pocv(df)
+        ocv = pocv(s)
+        q1 = q[findfirst(>=(v1), ocv)]
+        q2 = q[findfirst(>=(v2), ocv)]
+        Δq = q2 - q1
+
+        # ecm
+        (; ode) = ecm
+        cap_ecm = ode.p[1]
+        δQ = cap / cap_ecm
+        ocv_ecm = focv(s .* δQ)
+        q1_ecm = q[findfirst(>=(v1), ocv_ecm)]
+        q2_ecm = q[findfirst(>=(v2), ocv_ecm)]
+        Δq_ecm = q2_ecm - q1_ecm
+
+        # gp
+        (; gp, dt) = gpm
+        ŝ = StatsBase.transform(dt.s, q)
+        i = zero.(ŝ)
+        x = GPPPInput(:ocv, RowVecs([ŝ i]))
+        ocv_gp = gp(x)
+        μ = StatsBase.reconstruct(dt.v, mean(ocv_gp))
+        σ = StatsBase.reconstruct(dt.σ, sqrt.(var(ocv_gp)))
+
+        q1μ = q[findfirst(>=(v1), μ)]
+        q1σ = q1μ - q[findfirst(>=(v1), μ + σ)]
+        q1_gpm = q1μ ± q1σ
+
+        q2μ = q[findfirst(>=(v2), μ)]
+        q2σ = q[findfirst(>=(v2), μ - σ)] - q2μ
+        q2_gpm = q2μ ± q2σ
+
+        Δq_gpm = q2_gpm - q1_gpm
+
+        # results
+        (; id, Δq, Δq_ecm, Δq_gpm)
+    end |> DataFrame
+
+
+    # SOH
+    soh = [calc_capa_cccv(data[id]) / 4.8 for id in ids]
+    res[!, :soh] = soh .* 100
+    res[!, :Δsoh_ecm] = ([ecms[id].ode.p[1] / 4.8 for id in ids] - soh) .* 100
+
+    # OCV based SOH
+    Δq0 = res[1, :Δq]
+    res[!, :soh´] = res[:, :Δq] ./ Δq0 .* 100
+    res[!, :Δsoh´_ecm] = (res[:, :Δq_ecm] - res[:, :Δq]) ./ Δq0 .* 100
+    res[!, :Δsoh´_gpm] = (res[:, :Δq_gpm] - res[:, :Δq]) ./ Δq0 .* 100
+
+    # 
+    select!(res, [:id, :soh, :Δsoh_ecm, :soh´, :Δsoh´_ecm, :Δsoh´_gpm])
+    return res
 end
-
